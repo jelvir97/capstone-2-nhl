a5a3c184a879d9788288ba5364f288ba0fabbe3d
Juan Elvir <jelvir97@gmail.com>
2024-02-22 08:57:43 -0700

    google strategy defined

--- middleware/GoogleAuth.js
+++ middleware/GoogleAuth.js
@@ -1,14 +1,49 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} =  require('../secrets/google_client_secret.json').web
+const passport = require('passport')
+const User = require('../models/users')
 
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
+    clientID: process.env['GOOGLE_CLIENT_ID'],
+    clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+    callbackURL: '/oauth2/redirect/google',
   },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+
+  async function(request, accessToken, refreshToken, profile, done){
+    //console.log(profile) 
+    try{
+      const user  =  await User.get(profile.id)
+      console.log('user found', user.firstName)
+      done(null, user)
+    }catch(err){
+      console.log('ERRRORRR', err)
+      const user = await User.register({google_id:profile.id, 
+                                        firstName:profile.name.givenName, 
+                                        lastName:profile.name.familyName, 
+                                        email:profile.emails[0].value})
+
+      done(null,user)
+    }
   }
 ));
+
+passport.serializeUser(function(user, done) {
+  try{
+    done(null,user.google_id)
+  }catch(err){
+    done(err)
+  }
+  
+});
+
+passport.deserializeUser(async function(id, done) {
+
+  try{
+    const user = await User.get(id)
+    console.log(user)
+    done(null,user)
+  }catch(err){
+    done(err)
+  }
+});
+
+module.exports = passport;
