ac53ef4f60311fb0279e742d250a5f45bdfafa22
Juan Elvir <jelvir97@gmail.com>
2024-02-28 15:47:12 -0700

    adjusting port

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const COOKIE_KEYS = process.env.COOKIE_KEYS
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app')
 
-app.listen(3000, ()=>{
-    console.log("Listening on port 3000")
+const {PORT} = require('./config')
+
+app.listen(PORT, ()=>{
+    console.log("Listening on port " + PORT)
 })
