7156a7fd5d523c4b7cee03306d5f64abdce3fda5
Juan Elvir <jelvir97@gmail.com>
2024-02-28 09:48:30 -0700

    NHL_API model

---
+++ models/NHL_API.js
@@ -0,0 +1,118 @@
+"use strict"
+
+const axios = require('axios')
+
+const {
+    NotFoundError,
+    BadRequestError,
+    UnauthorizedError,
+  } = require("../expressError");
+
+/**
+ *  For routing nhl api requests through server.
+ * 
+ *  https://api.nhle.com
+ *  https://api-web.nhle.com
+ * 
+ *  ****These do not support CORS****
+ */
+class NHL_API {
+
+    /**
+     * Gets schedule for 'now'
+     * 
+     *  or date passed in
+     *  
+     *  date must be string : 'YYYY-MM-DD'
+     */
+    static async getSchedule(date='now'){
+        const {data} = await axios.get(`https://api-web.nhle.com/v1/schedule/${date}`)
+
+        return data
+    }
+
+    /**
+     * 
+     * Gets list of teams
+     * 
+     *   Takes no arguments
+     */
+    static async getTeamsList(){
+        const {data} = await axios.get(`https://api.nhle.com/stats/rest/en/team`)
+        return data
+    }
+
+    /**
+     * Gets standings data for entire league
+     * 
+     * returns mapped object organized into conferences/divisions
+     */
+    static async getStandings (){
+        const {data} = await axios.get(`https://api-web.nhle.com/v1/standings/now`)
+        const {standings} = data
+        const obj ={
+            'Western': {
+                'Pacific':[],
+                'Central': [],
+            },
+            'Eastern':{
+                'Atlantic':[],
+                'Metropolitan':[]
+            }
+        }
+
+        standings.forEach(team => {
+            obj[team.conferenceName][team.divisionName].push(team)
+        });
+
+        return obj;
+    }
+
+    /**
+     * Gets player stats for team
+     * 
+     * Must pass in team abreviation
+     */
+    static async getTeamStats(team){
+        if(!team) throw new BadRequestError('Must have team abbreviation')
+        const {data} = await axios.get(`https://api-web.nhle.com/v1/club-stats/${team}/now`)
+        
+        return data;
+    }
+
+    /**
+     * Gets current team roster
+     * 
+     * Must pass in team abreviation
+     */
+    static async getTeamSchedule(team, date='now'){
+        if(!team) throw new BadRequestError('Must have team abbreviation')
+        const {data} =  await axios.get(`https://api-web.nhle.com/v1/club-schedule/${team}/week/${date}`)
+        return data;
+    }
+
+    /**
+     * Gets current team roster
+     * 
+     * Must pass in team abreviation
+     */
+    static async getTeamRoster(team){
+        if(!team) throw new BadRequestError('Must have team abbreviation')
+        const {data} = await axios.get(`https://api-web.nhle.com/v1/roster/${team}/current`)
+        return data;
+    }
+
+    /**
+     *  Gets Play-by-play data for a game;
+     *  
+     *  Must pass in gameID
+     */
+    static async getPlayByPlay(gameID){
+        if(!gameID) throw new BadRequestError('Must have gameID')
+        const {data} = await axios.get(`https://api-web.nhle.com/v1/gamecenter/${gameID}/play-by-play`)
+        return data;
+    }
+
+}
+
+module.exports = NHL_API;
