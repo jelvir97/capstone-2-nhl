52ba12198753ff2f349fe784780ad8142980725f
Juan Elvir <jelvir97@gmail.com>
2024-03-07 14:23:28 -0700

    fix attempt

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express()
 app.use(express.json())
 
 app.use(session({
-    secret: 'cookie_secret',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
 }));
