1946214d921e234b20199e94fb654b4750e0eef5
Juan Elvir <jelvir97@gmail.com>
2024-03-07 12:13:57 -0700

    logging in /login/success

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post('/logout', function(req, res, next) {
  * Endpoint for React app to get user info after authentication.
  */
 router.get('/login/success', (req, res, next)=>{
-  console.log('/login/success',req.user)
+  console.log('/login/success',req.session.user)
   if (req.user) {
     res.json({
      message : "User Authenticated",
